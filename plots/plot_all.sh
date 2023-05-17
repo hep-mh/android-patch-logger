0017f4585e030b74990e0ed78b4ec8d2ef07497f
hep-mh <mail@hep-mh.com>
2023-05-17 18:48:57 +0200

    merged pdf

--- plots/plot_all.sh
+++ plots/plot_all.sh
@@ -4,3 +4,5 @@ python3 plot_patch_history.py Samsung-Galaxy-Tab-S6 2019-08-01
 python3 plot_patch_history.py Samsung-Galaxy-Note-10+ 2019-08-23
 python3 plot_patch_history.py Google-Pixel-6 2021-10-28
 python3 plot_patch_history.py Samsung-Galaxy-Tab-S8+ 2022-02-22
+
+pdfjam *.pdf --nup 3x2 --landscape --outfile all.pdf
